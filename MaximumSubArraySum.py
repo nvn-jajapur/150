22ced649d90be0b0b8b715d48b090cf02406aaf4
nvn-jajapur <naveen.jajapur-1@ou.edu>
2025-05-27 18:13:39 -0500

    Update MaximumSubArraySum.py
    Added the logic if the position is asked

--- MaximumSubArraySum.py
+++ MaximumSubArraySum.py
@@ -1,28 +1,32 @@
 
-# Kadanes Algorithm
+#### Kadanes Algorithm ######
 '''
 Here in this algorithm, we take the sum variable and only check for the positive sum  
 If we encounter -ves, then we start again the sum for 0
 If we encounter +ves, then we add the sum and which would be resultant answer
 '''
 import sys
-def maximum_sub_array_sum(arr:[])->int:
+def maximum_sub_array_sum(arr:[])->[]:
     sum=0
-    start,end=0,-1
-    ansStart=-1
-    maxSum=-sys.maxsize-1
-    for i,v in enumerate(arr):
-        sum+=v
-        if sum==0:
+    maxSum = -sys.maxsize-1
+    ansStart,end=-1,-1
+    start=0
+
+    for i,v in enumerate((arr)):
+        if (sum==0):
             start=i
+        sum+=v
         if sum>maxSum:
-            maxSum =sum
+            maxSum=sum
             ansStart=start
             end=i
-        if(sum<0):
+        if sum<0:
             sum=0
-    return maxSum
-
+    return [maxSum,ansStart,end]
+'''
+Time Complexity : O(N)
+Space Complexity : O(1)
+'''
 # If only sum is asked without the positions
 def maximum_sub_array_sum1(arr:[])->int:
     sum=0
@@ -36,7 +40,7 @@ def maximum_sub_array_sum1(arr:[])->int:
     return maxSum
 
 if __name__=="__main__":
-    arr = [-1,-2,0,0,8,-1,0]
+    arr = [-1,-2,1,0,0,8,-1,0]
     print(maximum_sub_array_sum(arr))
     print(maximum_sub_array_sum1(arr))
 '''
